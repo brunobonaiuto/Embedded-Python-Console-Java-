e9753069c35fbf60711923c0b97bc891474a83c7
bbbolivar <bonaiuto@esteco.com>
2024-08-07 17:58:13 +0200

    inputLine b= 20, runLine method works perfectly when an assigment is passed

--- src/test/java/org/example/python/PyRunnerTest.java
+++ src/test/java/org/example/python/PyRunnerTest.java
@@ -11,20 +11,46 @@ class PyRunnerTest {
 
     @Test
     void testRunLineIsEmpty() {
+        String input = "";
+        PyRunner pyRunner = new PyRunner();
+        //
+        String result = pyRunner.runLine(input);
+        //
+        assertEquals("", result);
+    }
+
+    @Test
+    void testRunLineSingleCharThrows() {
+        String inputLineFromConsole = "b";
+        PyRunner pyRunner = new PyRunner();
+        //
+        //executeCodeModule should throw
+        assertThrows(IllegalArgumentException.class, () -> pyRunner.runLine(inputLineFromConsole));
+    }
+
+    @Test
+    void testRunLineSingleCharThrowsCase2() {
+        String inputLineFromConsole = " -";
+        PyRunner pyRunner = new PyRunner();
+        //
+        //executeCodeModule should throw
+        assertThrows(IllegalArgumentException.class, () -> pyRunner.runLine(inputLineFromConsole));
+    }
+
+    @Test
+    void testRunLineContainsAnAssigment() {
+        String input = "b = 20";
         String expected = "";
         PyRunner pyRunner = new PyRunner();
         //
-        String result = pyRunner.runLine("");
+        String result = pyRunner.runLine(input);
         //
         assertEquals(expected, result);
     }
 
-    @Test
-    void testRunLineSingleChar() {
-        String inputLineFromConsole = "b";
-        PyRunner pyRunner = new PyRunner();
-        //
-        assertThrows(IllegalArgumentException.class, () -> pyRunner.runLine(inputLineFromConsole));
-    }
+
+
+
+
 
 }
