73289765b69e4653af24753f08b4844fc493904c
bbbolivar <bonaiuto@esteco.com>
2024-08-07 18:03:39 +0200

    runLine called two times (NoWorking)

--- src/test/java/org/example/python/PyRunnerTest.java
+++ src/test/java/org/example/python/PyRunnerTest.java
@@ -7,8 +7,6 @@ import static org.junit.jupiter.api.Assertions.assertThrows;
 
 
 class PyRunnerTest {
-
-
     @Test
     void testRunLineIsEmpty() {
         String input = "";
@@ -47,6 +45,24 @@ class PyRunnerTest {
         //
         assertEquals(expected, result);
     }
+
+    @Test
+    void testRunLineAccessingVariable() {
+        String inputOne = "b = 20";
+        String expectedOne = "";
+        String inputTwo = "b";
+        String expectedTwo = "20";
+        PyRunner pyRunner = new PyRunner();
+        //
+        String result1 = pyRunner.runLine(inputOne);
+        String result2 = pyRunner.runLine(inputTwo);
+        //
+        assertEquals(expectedOne, result1);
+        assertEquals(expectedTwo, result2);
+
+    }
+
+
 
 
 
