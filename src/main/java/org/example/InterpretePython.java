f230ad19ec6c916b49c02988967af97356c868a1
bbbolivar <bonaiuto@esteco.com>
2024-08-09 09:30:07 +0200

    while loop in start method

--- src/main/java/org/example/InterpretePython.java
+++ src/main/java/org/example/InterpretePython.java
@@ -1,15 +1,28 @@
 package org.example;
 
-import org.example.python.PyObject;
+import org.example.python.PyRunner;
+
+import java.util.Scanner;
 
 public class InterpretePython {
+    private PyRunner pyRunner;
 
+    public InterpretePython() {
+        pyRunner = new PyRunner();
+    }
 
     public void start() {
+        Scanner scanner = new Scanner(System.in);
         System.out.print("Welcome to Python\n");
         System.out.print(">>> ");
-//        String line = scanner.nextLine();
-//        System.out.println(">>>"+ resultFromRunner);
+        String scan = scanner.nextLine();
+        while(!scan.equals("exit")){
+            String result = pyRunner.runLine(scan);
+            System.out.println(result);
+            System.out.print(">>> ");
+            scan = scanner.nextLine();
+        }
+
     }
 
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-
+        InterpretePython interpretePython = new InterpretePython();
+        interpretePython.start();
     }
 }
