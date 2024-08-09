3b32aec8ef9f87f3432d94c59697250e2b6f9e4b
bbbolivar <bonaiuto@esteco.com>
2024-08-09 14:40:25 +0200

    refactoring of the interpreterPython

--- src/main/java/org/example/InterpreterPython.java
+++ src/main/java/org/example/InterpreterPython.java
@@ -7,38 +7,73 @@ import java.util.Scanner;
 public class InterpreterPython {
     private final PyRunner pyRunner;
     private final Scanner scanner;
+    private String input;
+    public static final String LINE_SYMBOL = ">>> ";
+    public static final String STATEMENT_SYMBOL = "...\t";
+    public static final String EXIT_COMMAND = "exit()";
+    public static final String FUNCTION_SYMBOL = ":";
 
     public InterpreterPython() {
         pyRunner = new PyRunner();
         scanner = new Scanner(System.in);
     }
+
     public void start() {
-        StringBuilder stringBuilder = new StringBuilder();
-        System.out.println("Welcome to Python");
-        System.out.print(">>> ");
-        String scan = scanner.nextLine();
-        while(!scan.equals("exit")){
-            if(scan.contains(":")){
-                while(!scan.isEmpty()) {
-                    stringBuilder.append(scan + "\n");
-                    System.out.print("...\t");
-                    scan = scanner.nextLine();
-                }
-                scan = stringBuilder.toString();
-            }
-            runLine(scan);
-            scan = scanner.nextLine();
-        }
+        welcomeMessage();
+        input = getInputFromConsole();
+        evaluateInput();
         pyRunner.quit();
     }
 
-    private void runLine(String scan) {
-        String result = pyRunner.runLine(scan);
+    private void welcomeMessage() {
+        printMessage("Welcome to Python\n");
+        printMessage(LINE_SYMBOL);
+        //System.out.print(LINE_SYMBOL);
+    }
+
+    private void printMessage(String output) {
+        System.out.print(output);
+    }
+
+    private String getInputFromConsole() {
+        return scanner.nextLine();
+    }
+
+    private void evaluateInput() {
+        StringBuilder stringBuilder = new StringBuilder();
+        while(!input.equals(EXIT_COMMAND)){
+            if(inputIsFunction()){
+                defineFunction(stringBuilder);
+            }
+            runLine();
+            input = getInputFromConsole();
+        }
+    }
+
+    private boolean inputIsFunction() {
+        return input.endsWith(FUNCTION_SYMBOL);
+    }
+
+    private String defineFunction(StringBuilder stringBuilder) {
+        while(!input.isEmpty()) {
+            stringBuilder.append(input).append("\n");
+            printMessage(STATEMENT_SYMBOL);
+            //System.out.print(STATEMENT_SYMBOL);
+            input = getInputFromConsole();
+        }
+        input = stringBuilder.toString();
+        return input;
+    }
+
+    private void runLine() {
+        String result = pyRunner.runLine(input);
         if(result.isBlank()){
-            System.out.print(">>> ");
+            printMessage(LINE_SYMBOL);
+            //System.out.print(LINE_SYMBOL);
         }else {
-            System.out.println(result);
-            System.out.print(">>> ");
+            printMessage(result + "\n");
+            printMessage(LINE_SYMBOL);
+            //System.out.print(LINE_SYMBOL);
         }
     }
 }
