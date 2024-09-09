081d6a40866fe7fa02ed7931ab8ad03c9a335bec
bbbolivar <bonaiuto@esteco.com>
2024-09-09 09:00:38 +0200

    small change in main to run from console

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,8 @@ import org.example.interprete.engine.EmbeddedPython;
 import org.example.GUI.InterpreteGui;
 import org.example.GUI.frames.mainFrame.io.InputPanel;
 import org.example.GUI.frames.mainFrame.io.OutputPanel;
+import org.example.interprete.io.InputConsole;
+import org.example.interprete.io.OutputConsole;
 
 import javax.swing.*;
 
@@ -12,13 +14,13 @@ import javax.swing.*;
 public class Main {
     public static void main(String[] args) {
         //create input and output from outside
-//        Output outputPanel = new DefaultOutput();
-//        Input inputPanel = new DefaultInput();
+//       Input inputPanel = new InputConsole();
+//       Output outputPanel  = new OutputConsole();
 
         Output outputPanel = new OutputPanel();
         Input inputPanel = new InputPanel(outputPanel);
 
-        SwingUtilities.invokeLater(() -> InterpreteGui.initializeWindowComponents(inputPanel, outputPanel));
+        //SwingUtilities.invokeLater(() -> InterpreteGui.initializeWindowComponents(inputPanel, outputPanel));
 
         EmbeddedPython embeddedPython = new EmbeddedPython(inputPanel,outputPanel);
         embeddedPython.initialize();
